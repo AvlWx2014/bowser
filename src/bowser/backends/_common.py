a3224ffa8829525b99f6ede9993d469a195ea56a
Ryan Smith <ryan.d.smith.106@gmail.com>
2024-02-10 12:29:08 -0500

    If a metadata file exists, but is empty, return no metadata

--- src/bowser/backends/_common.py
+++ src/bowser/backends/_common.py
@@ -8,5 +8,7 @@ def get_metadata_for_file(path: Path) -> Mapping[str, Any]:
     metadata_path = path.with_suffix(".metadata")
     if not metadata_path.exists():
         return {}
-    with metadata_path.open("r") as in_:
-        return json.load(in_)
+    input_ = metadata_path.read_text()
+    if not input_:
+        return {}
+    return json.loads(input_)
