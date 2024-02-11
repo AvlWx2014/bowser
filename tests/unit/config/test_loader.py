61914cb7652bfdf99a21ff7e66b5d552ebf2d37e
Ryan Smith <ryan.d.smith.106@gmail.com>
2024-02-11 12:56:34 -0500

    Add a test case for _merge_configuration

--- tests/unit/config/test_loader.py
+++ tests/unit/config/test_loader.py
@@ -1,10 +1,13 @@
+from collections.abc import Mapping
 from pathlib import Path
+from typing import Any
 
+import pytest
 from hamcrest import assert_that, equal_to
 
 from bowser.config.backend.aws import AwsS3BowserBackendConfig, Bucket
 from bowser.config.base import BowserConfig
-from bowser.config.loader import load_app_configuration
+from bowser.config.loader import _merge_configuration, load_app_configuration
 
 HERE = Path(__file__).parent
 DATA = HERE / "data"
@@ -23,4 +26,60 @@ def test_load_app_configuration():
         ],
     )
     actual = load_app_configuration(check_paths=(DATA,))
+    assert_that(actual, equal_to(expected))
+
+
+@pytest.mark.parametrize(
+    "left,right,expected",
+    [
+        (
+            {
+                "key1": "left",
+                "key2": [1, 2, 3, 4],
+                "key3": {"nested1": "left", "nested2": "left"},
+            },
+            {},
+            {
+                "key1": "left",
+                "key2": [1, 2, 3, 4],
+                "key3": {"nested1": "left", "nested2": "left"},
+            },
+        ),
+        (
+            {},
+            {
+                "key1": "right",
+                "key2": [1, 2, 3, 4],
+                "key3": {"nested1": "right", "nested2": "right"},
+            },
+            {
+                "key1": "right",
+                "key2": [1, 2, 3, 4],
+                "key3": {"nested1": "right", "nested2": "right"},
+            },
+        ),
+        (
+            {
+                "key1": "left-only",
+                "key2": [1, 2, 3, 4],
+                "key3": {"nested1": "left", "nested2": "left"},
+            },
+            {
+                "key4": "right-only",
+                "key2": [4, 3, 2, 1],
+                "key3": {"nested1": "right"},
+            },
+            {
+                "key1": "left-only",
+                "key2": [4, 3, 2, 1],
+                "key3": {"nested1": "right", "nested2": "left"},
+                "key4": "right-only",
+            },
+        ),
+    ],
+)
+def test__merge_configuration(
+    left: Mapping[str, Any], right: Mapping[str, Any], expected: Mapping[str, Any]
+):
+    actual = _merge_configuration(left, right)
     assert_that(actual, equal_to(expected))
