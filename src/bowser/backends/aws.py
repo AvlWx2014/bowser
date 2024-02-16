f47f1774b35d4fd21d73705e7c4e42efa07d727b
Ryan Smith <ryan.d.smith.106@gmail.com>
2024-02-16 12:39:14 -0500

    Make metadata object tags instead of metadata.

--- src/bowser/backends/aws.py
+++ src/bowser/backends/aws.py
@@ -3,6 +3,7 @@ import os
 from collections.abc import Mapping, MutableSequence
 from pathlib import Path
 from typing import TYPE_CHECKING, Any, NamedTuple
+from urllib.parse import urlencode
 
 from ..config.backend.aws import AwsS3BowserBackendConfig
 from ._common import get_metadata_for_file
@@ -10,6 +11,9 @@ from .base import BowserBackend
 
 if TYPE_CHECKING:
     from mypy_boto3_s3 import S3Client
+
+
+AWS_TAG_MAXIMUM = 10
 
 
 class _FileMetadataPair(NamedTuple):
@@ -52,12 +56,22 @@ class AwsS3Backend(BowserBackend):
                 relative_path = path.relative_to(source.parent)
                 # lstrip to remove any unwanted leading "/" e.g. if `bucket.key` is empty
                 key = f"{bucket.key}/{relative_path!s}".lstrip("/")
-                logging.debug("Uploading %s to %s/%s", path, bucket.name, key)
+                tags = _convert_metadata_to_s3_object_tags(meta)
+                logging.info("Uploading %s to %s/%s", path, bucket.name, key)
                 self._client.put_object(
                     Body=str(path),
                     Bucket=bucket.name,
                     Key=key,
-                    Metadata=meta,  # TODO: metadata as tags instead of metadata?
+                    Tagging=tags,
                     ChecksumAlgorithm="SHA256",
                 )
-                logging.debug("Done.")
+                logging.info("Done.")
+
+
+def _convert_metadata_to_s3_object_tags(
+    metadata: Mapping[str, Any], limit: int = AWS_TAG_MAXIMUM
+) -> str:
+    limit = max(limit, 1)
+    pairs = tuple(metadata.items())
+    stringified = tuple((_, str(v)) for _, v in pairs)
+    return urlencode(stringified[:limit])
