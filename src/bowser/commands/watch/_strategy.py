dfc7f234b5e185e01b6230f8882d5dfcd67c176b
Ryan Smith <ryan.d.smith.106@gmail.com>
2024-02-21 11:43:57 -0500

    WatchStrategy becomes marker interface extending ObservableTransformer

--- src/bowser/commands/watch/_strategy.py
+++ src/bowser/commands/watch/_strategy.py
@@ -1,63 +1,10 @@
-from abc import ABC, abstractmethod
-from pathlib import Path
+import logging
 
-from ._event import Event
+from ...extensions.rx import ObservableTransformer
+from ...inotify import InotifyEventData
 
-
-class WatchStrategy(ABC):
-    def __init__(self, watch_root: Path) -> None:
-        self._root = watch_root
-
-    @abstractmethod
-    def on_next(self, event: Event) -> None: ...
-
-    @abstractmethod
-    def should_stop(self) -> bool: ...
-
-    @property
-    @abstractmethod
-    def reason(self) -> str: ...
+LOGGER = logging.getLogger("bowser")
 
 
-class SentinelWatchStrategy(WatchStrategy):
-
-    def __init__(self, watch_root: Path, *, sentinel: str = ".bowser.complete") -> None:
-        super().__init__(watch_root)
-        self._sentinel = sentinel
-
-    def on_next(self, event: Event) -> None:
-        pass
-
-    def should_stop(self) -> bool:
-        return (self._root / self._sentinel).exists()
-
-    @property
-    def reason(self) -> str:
-        return f"Sentinel file {self._root / self._sentinel} found."
-
-
-class CountWatchStrategy(WatchStrategy):
-    def __init__(self, watch_root: Path, *, limit: int) -> None:
-        super().__init__(watch_root)
-        self._limit = limit
-        self._count = 0
-
-    def on_next(self, event: Event) -> None:
-        match event:
-            case Event.COMPLETION:
-                if self._count < self._limit:
-                    self._count += 1
-            case _:
-                raise RuntimeError(
-                    "Exhaustive match on enum type 'Event' failed to match."
-                    f"Unknown value: {event}"
-                )
-
-    def should_stop(self) -> bool:
-        return self._count == self._limit
-
-    @property
-    def reason(self) -> str:
-        return (
-            f"{self._count} / {self._limit} trees signaled themselves as upload ready."
-        )
+class WatchStrategy(ObservableTransformer[InotifyEventData]):
+    pass
