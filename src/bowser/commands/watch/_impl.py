4d0b3edd3c6023927bdb00b2ff3af4155dc16414
Ryan Smith <ryan.d.smith.106@gmail.com>
2025-02-13 08:23:47 -0500

    commands/watch: Update watch command implementation to use Watchdog
    Rewrite the downstream Observer to work on events from Watchdog. Reorganize the actual implementation of the watch command to make sure watchdog is started and stopped appropriately.

--- src/bowser/commands/watch/_impl.py
+++ src/bowser/commands/watch/_impl.py
@@ -6,13 +6,49 @@ from threading import Condition
 
 from reactivex import operators as ops
 from reactivex.abc import DisposableBase, ObserverBase
-from reactivex.scheduler import EventLoopScheduler, ThreadPoolScheduler
+from reactivex.scheduler import ThreadPoolScheduler
+from watchdog.events import FileCreatedEvent
+from watchdog.observers import Observer as FileSystemEventLoop
 
 from ...backends.base import BowserBackend
-from ...inotify import InotifyEvent, InotifyEventData, observable_inotifywait
+from ...extensions.watchdog import WatchdogEventObservable
 from ._strategy import WatchStrategy
 
 LOGGER = logging.getLogger("bowser")
+
+
+class Terminus(ObserverBase[FileCreatedEvent], DisposableBase):
+    def __init__(
+        self, backends: Collection[BowserBackend], on_dispose: Condition
+    ) -> None:
+        self._on_dispose = on_dispose
+        self._backends: Collection[BowserBackend] = backends
+
+    def on_completed(self) -> None:
+        LOGGER.debug("Terminus#on_completed: upstream completed.")
+        self.dispose()
+
+    def on_error(self, error: Exception) -> None:
+        LOGGER.error("Unhandled exception in upstream Observable.", exc_info=error)
+        self.dispose()
+
+    def on_next(self, value: FileCreatedEvent) -> None:
+        if (as_path := Path(value.src_path)).name == ".bowser.ready":
+            for backend in self._backends:
+                try:
+                    backend.upload(as_path.parent)
+                except Exception:
+                    LOGGER.exception(
+                        "Unhandled exception in upload for backend '%s'",
+                        backend.__class__.__name__,
+                    )
+                    LOGGER.info(
+                        "Will attempt other backends (if any) and continue processing events..."
+                    )
+
+    def dispose(self) -> None:
+        with self._on_dispose:
+            self._on_dispose.notify_all()
 
 
 def execute(
@@ -20,52 +56,33 @@ def execute(
     backends: Collection[BowserBackend],
     transform: WatchStrategy,
 ) -> None:
-    completed = Condition()
-
-    class AnonymousObserver(ObserverBase[InotifyEventData], DisposableBase):
-
-        def on_next(self, value: InotifyEventData) -> None:
-            nonlocal backends
-            if InotifyEvent.CREATE not in value.events:
-                return
-
-            if value.subject == ".bowser.ready":
-                LOGGER.debug("Sentinel file found in %s", value.watch)
-                for backend in backends:
-                    try:
-                        backend.upload(value.watch)
-                    except Exception:
-                        LOGGER.exception("Unhandled exception in backend upload.")
-                        LOGGER.info("Observer will continue processing events...")
-
-        def on_error(self, error: Exception) -> None:
-            LOGGER.error("Unhandled exception in upstream Observable.", exc_info=error)
-            self.dispose()
-
-        def on_completed(self) -> None:
-            self.dispose()
-
-        def dispose(self) -> None:
-            LOGGER.debug("Signaling completion from Observer.")
-            with completed:
-                completed.notify()
-
     cpus = os.cpu_count() or 1
     workers = max(cpus, 1)
     scheduler = ThreadPoolScheduler(max_workers=workers)
+
+    completed = Condition()
+    origin = WatchdogEventObservable()
+    terminus = Terminus(backends, on_dispose=completed)
     # Subscribe to the source Observable. The upstream source Observable waits for
     # subscription before events start flowing. Upstream work will happen on a dedicated thread
     # scheduled by the scheduler passed to the Observable factory function below.
     (
         # wrap the source Observable in our watch strategy transformer
-        transform(observable_inotifywait(root, scheduler=EventLoopScheduler()))
+        transform(origin)
         .pipe(
             # run the observer actions on the ThreadPoolScheduler
             ops.observe_on(scheduler),
         )
-        .subscribe(AnonymousObserver())
+        .subscribe(terminus)
     )
+
+    loop = FileSystemEventLoop()
+    loop.schedule(origin, str(root), recursive=True)
+    loop.start()
 
     # wait on the main thread until the Observer has been disposed of
     with completed:
         completed.wait()
+
+    loop.stop()
+    loop.join()
--- tests/unit/commands/test_watch.py
+++ tests/unit/commands/test_watch.py
@@ -1,3 +1,4 @@
+from collections.abc import Iterator
 from pathlib import Path
 from unittest.mock import MagicMock, patch
 
@@ -5,7 +6,8 @@ import pytest
 import reactivex
 from hamcrest import assert_that, equal_to
 from reactivex import Observable
-from reactivex import operators as ops
+from watchdog.events import FileCreatedEvent
+from watchdog.observers import Observer as FileSystemEventLoop
 
 from bowser.backends.base import BowserBackend
 from bowser.commands.watch import CountWatchStrategy, SentinelWatchStrategy, execute
@@ -26,15 +28,30 @@ def mock_tree(tmp_path: Path) -> Path:
 
 
 @pytest.fixture
-def mock_observable_for_sentinel_strategy(mock_tree: Path) -> Observable[bytes]:
+def mock_watchdog_observer() -> Iterator[FileSystemEventLoop]:
+    with patch(
+        "bowser.commands.watch._impl.FileSystemEventLoop", spec=FileSystemEventLoop
+    ) as mock_:
+        yield mock_
+        # mock_.schedule.assert_called()
+        # mock_.start.assert_called()
+        # mock_.stop.assert_called()
+
+
+@pytest.fixture
+def mock_observable_for_sentinel_strategy(
+    mock_tree: Path,
+) -> Observable[FileCreatedEvent]:
     return reactivex.of(
-        f"{mock_tree / 'test1'} CREATE .bowser.ready",
-        f"{mock_tree} CREATE .bowser.complete",
-    ).pipe(ops.map(lambda line: line.encode()))
+        FileCreatedEvent(src_path=str(mock_tree / "test1" / ".bowser.ready")),
+        FileCreatedEvent(src_path=str(mock_tree / ".bowser.complete")),
+    )
 
 
 def test_watch_command_sentinel_strategy(
-    mock_tree: Path, mock_observable_for_sentinel_strategy
+    mock_tree: Path,
+    mock_observable_for_sentinel_strategy: Observable[FileCreatedEvent],
+    mock_watchdog_observer: FileSystemEventLoop,
 ):
     """Test the watch command with as near to real functionality as is pragmatic.
 
@@ -42,12 +59,8 @@ def test_watch_command_sentinel_strategy(
     thread to perform the file operations that will make execution stop.
     """
     mock_backends = [MagicMock(spec=BowserBackend) for _ in range(3)]
-    with patch(
-        "bowser.inotify._observable.observable_background_process"
-    ) as mock_observable_background_process:
-        mock_observable_background_process.return_value = (
-            mock_observable_for_sentinel_strategy
-        )
+    with patch("bowser.commands.watch._impl.WatchdogEventObservable") as mock_upstream:
+        mock_upstream.return_value = mock_observable_for_sentinel_strategy
         execute(
             mock_tree,
             backends=mock_backends,
@@ -58,16 +71,18 @@ def test_watch_command_sentinel_strategy(
 
 
 @pytest.fixture
-def mock_observable_for_count_strategy(mock_tree: Path) -> Observable[bytes]:
+def mock_observable_for_count_strategy(mock_tree: Path) -> Observable[FileCreatedEvent]:
     return reactivex.of(
-        f"{mock_tree / 'test1'} CREATE .bowser.ready",
-        f"{mock_tree / 'test2'} CREATE .bowser.ready",
-        f"{mock_tree / 'test3'} CREATE .bowser.ready",
-    ).pipe(ops.map(lambda line: line.encode()))
+        FileCreatedEvent(src_path=str(mock_tree / "test1" / ".bowser.ready")),
+        FileCreatedEvent(src_path=str(mock_tree / "test2" / ".bowser.ready")),
+        FileCreatedEvent(src_path=str(mock_tree / "test3" / ".bowser.ready")),
+    )
 
 
 def test_watch_command_count_strategy(
-    mock_tree: Path, mock_observable_for_count_strategy
+    mock_tree: Path,
+    mock_observable_for_count_strategy: Observable[FileCreatedEvent],
+    mock_watchdog_observer: FileSystemEventLoop,
 ):
     """Test the watch command using a count strategy.
 
@@ -78,11 +93,7 @@ def test_watch_command_count_strategy(
     """
     n = 3
     mock_backend = MagicMock(spec=BowserBackend)
-    with patch(
-        "bowser.inotify._observable.observable_background_process"
-    ) as mock_observable_background_process:
-        mock_observable_background_process.return_value = (
-            mock_observable_for_count_strategy
-        )
+    with patch("bowser.commands.watch._impl.WatchdogEventObservable") as mock_upstream:
+        mock_upstream.return_value = mock_observable_for_count_strategy
         execute(mock_tree, backends=[mock_backend], transform=CountWatchStrategy(n=n))
     assert_that(mock_backend.upload.call_count, equal_to(n))
