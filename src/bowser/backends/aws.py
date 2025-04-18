71a5080442311ea1a394d2ba3ef880e1f07a44ae
Ryan Smith <ryan.d.smith.106@gmail.com>
2025-04-18 12:57:37 -0400

    backends/aws: Clear the destination key before uploading
    This aims to make subsequent uploads of content from the same source idempotent.

--- src/bowser/backends/aws.py
+++ src/bowser/backends/aws.py
@@ -2,18 +2,20 @@ import logging
 import os
 from collections.abc import Mapping, MutableSequence
 from pathlib import Path
-from typing import TYPE_CHECKING, Any, NamedTuple
+from typing import TYPE_CHECKING, Any, NamedTuple, cast
 from urllib.parse import urlencode
+
+from ..result import Result
 
 if TYPE_CHECKING:
     from mypy_boto3_s3 import S3ServiceResource
 
-from ..config.backend.aws import AwsS3BowserBackendConfig
+from ..config.backend.aws import AwsS3BowserBackendConfig, Bucket
+from ..config.link import Link
 from ._common import get_metadata_for_file
 from .base import BowserBackend
 
 LOGGER = logging.getLogger("bowser")
-
 
 AWS_TAG_MAXIMUM = 10
 
@@ -46,49 +48,74 @@ class AwsS3Backend(BowserBackend):
         not_metadata = not filename.endswith(".metadata")
         return not_bowser and not_metadata
 
+    def _clear_prefix(self, bucket: Bucket, prefix: str) -> Result[int]:
+        LOGGER.info(
+            "Clearing prefix %s...",
+            prefix,
+        )
+        s3bucket = self._s3.Bucket(bucket.name)
+        objects = s3bucket.objects.filter(Prefix=prefix)
+        try:
+            response = objects.delete()
+        except Exception as e:
+            return Result[int].failure(e)
+
+        deleted = sum(len(obj.get("Deleted", [])) for obj in response)
+        return Result[int].success(deleted)
+
+    def clear_prefix(self, bucket: Bucket, prefix: str) -> int:
+        result = self._clear_prefix(bucket, prefix)
+        if (exc := result.exception_or_none()) is not None:
+            raise exc
+
+        return cast(int, result.get_or_none())
+
     def upload(self, source: Path) -> None:
         """Upload files in the tree rooted at ``source`` to AWS S3.
 
         The name of ``source`` likewise becomes the root of the resulting object's key
         on S3.
         """
-        for_upload: MutableSequence[_FileMetadataPair] = []
-        for root, _, files in os.walk(source):
-            for file in filter(self._filter, files):
-                local = Path(root, file)
-                metadata: Mapping[str, Any] = get_metadata_for_file(local)
-                for_upload.append(_FileMetadataPair(local, metadata))
-
+        source_as_relative_path = source.relative_to(self.watch_root)
         for bucket in self._config.buckets:
+            source_prefix = f"{bucket.prefix}/{source_as_relative_path!s}"
+            # clear link
             if bucket.link is not None:
                 # first, for each bucket ensure any links are deleted before possibly being
                 # re-created
                 # assumes that links are somewhere on the path between the watch root and the tree
                 # rooted at `source`, which is the parent for all objects being uploaded
-                relative_path = source.relative_to(self.watch_root)
-                tree_prefix = f"{bucket.prefix}/{relative_path!s}".lstrip("/")
-                if bucket.link.target.matches(tree_prefix):
-                    link_prefix = bucket.link.substitute(tree_prefix)
-                    s3bucket = self._s3.Bucket(bucket.name)
-                    LOGGER.info(
-                        "Clearing link prefix %s before uploading any objects...",
-                        link_prefix,
-                    )
-                    objects = s3bucket.objects.filter(Prefix=link_prefix)
-                    response = objects.delete()
-                    deleted = sum(len(obj.get("Deleted", [])) for obj in response)
+                if bucket.link.target.matches(source_prefix):
+                    link_prefix = bucket.link.substitute(source_prefix)
+                    deleted = self.clear_prefix(bucket, link_prefix)
                     LOGGER.info(
                         "Removed %d objects from link prefix %s...",
                         deleted,
                         link_prefix,
                     )
 
-        for bucket in self._config.buckets:
             s3bucket = self._s3.Bucket(bucket.name)
+
+            # clear destination to make upload idempotent on retries
+            deleted = self.clear_prefix(bucket, source_prefix)
+            LOGGER.info(
+                "Removed %d objects from prefix %s...",
+                deleted,
+                source_prefix,
+            )
+
+            # resovle files for upload
+            for_upload: MutableSequence[_FileMetadataPair] = []
+            for root, _, files in os.walk(source):
+                for file in filter(self._filter, files):
+                    local = Path(root, file)
+                    metadata: Mapping[str, Any] = get_metadata_for_file(local)
+                    for_upload.append(_FileMetadataPair(local, metadata))
+
             for path, meta in for_upload:
-                relative_path = path.relative_to(self.watch_root)
+                source_as_relative_path = path.relative_to(self.watch_root)
                 # lstrip to remove any unwanted leading "/" e.g. if `bucket.prefix` is empty
-                key = f"{bucket.prefix}/{relative_path!s}".lstrip("/")
+                key = f"{bucket.prefix}/{source_as_relative_path!s}".lstrip("/")
                 tags = _convert_metadata_to_s3_object_tags(meta)
                 LOGGER.info("Uploading %s to %s/%s", path, bucket.name, key)
                 s3bucket.upload_file(
